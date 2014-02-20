a8c607874a05b0767214d5ac93f9a95484a32c1e
Michael Reiley <mreiley@pariahsoft.com>
2014-02-20 05:01:27 -0800

    DatabaseManager uses a custom format.

--- src/databasemanager.py
+++ src/databasemanager.py
@@ -24,20 +24,21 @@
 ## IN THE SOFTWARE.
 ## **********
 
-from dbm import dumb as dumbdbm
+import base64
 import os
-import sys
+import struct
 
 
 class DatabaseManager:
     """The Database Manager
 
-    Manages a simple database for storing persistent values, using Python's dumbdbm.
+    Manages a simple database for storing persistent values, using a custom format.
 
-    Args:
+    Attributes:
         driftwood: Base class instance.
+        filename: Filename of the database.
 
-    Note: The database only accepts string values.
+    Note: The database only accepts string keys and values.
     """
     def __init__(self, driftwood):
         """DatabaseManager class initializer.
@@ -47,51 +48,178 @@ class DatabaseManager:
         """
         self.driftwood = driftwood
 
-        self.__database = None
+        self.filename = os.path.join(self.driftwood.config["database"]["root"],
+                                     self.driftwood.config["database"]["name"])
 
-        self.__open(self.driftwood.config["database"]["name"])
+        if not os.path.exists(self.filename):
+            open(self.filename, "a+").close()
 
     def __contains__(self, item):
-        if item in self.__database:
+        if self.__get_pos(item)[0] >= 0:
             return True
         return False
 
     def __getitem__(self, item):
-        return self.__database[item]
+        return self.get(item)
 
     def __setitem__(self, item, value):
+        self.put(item, value)
+
+    def __delitem__(self, item):
+        self.remove(item)
+
+    def hash(self, string):
+        """Hash a string.
+
+        Perform a hashing function on a string. This is used to hash the keys in the database file.
+
+        Args:
+            string: The string to hash.
+
+        Returns: A 64-bit integer.
+        """
+        h = 0
+        string += "ohscaffy"
+
+        for i in range(len(string)):
+            h = (37 * h) + ord(string[i])
+        h += (h << 33)
+
+        return h % 2**64
+
+    def get(self, key):
+        """Get a value by key.
+
+        Retrieve a value from the database by its key.
+
+        Args:
+            key: The key whose value to retrieve.
+
+        Returns: String value of the key.
+        """
+        key = self.__test_str(key)
+        if not key:
+            return
+
+        with open(self.filename, "rb") as dbfile:
+            while True:
+                block = dbfile.read(10)
+                if not block:
+                    break
+
+                stored_key, sz = struct.unpack("<QH", block)
+
+                if self.hash(key) == stored_key:
+                    self.driftwood.log.info("Database", "get", "\"{0}\"".format(key))
+                    return base64.b64decode(dbfile.read(sz)).decode("utf-8")
+                else:
+                    dbfile.read(sz)
+
+        self.driftwood.log.msg("ERROR", "Database", "no such key", "\"{0}\"".format(key))
+
+    def put(self, key, value):
+        """Put a value by key.
+
+        Create or update a key with a new value.
+
+        Args:
+            key: The key to create or update.
+            value: The value to store.
+        """
+        key, value = self.__test_str(key), self.__test_str(value)
+        if not key or not value:
+            print(type(key))
+            return
+
+        origvalue = value
+        value = base64.b64encode(value.encode("utf-8"))
+        if len(value) > 65535:
+            self.driftwood.log.msg("ERROR", "Database", "value too long", "\"{0}\"".format(key))
+
+        pos, psz = self.__get_pos(key)
+
+        if pos >= 0:
+            with open(self.filename, "rb") as dbfile:
+                with open(self.filename+'~', "wb") as tmpfile:
+                    tmpfile.write(dbfile.read(pos))
+                    tmpfile.write(struct.pack("<QH", self.hash(key), len(value)))
+                    tmpfile.write(value)
+                    dbfile.seek(10+psz, 1)
+                    tmpfile.write(dbfile.read())
+
+            os.remove(self.filename)
+            os.rename(self.filename+'~', self.filename)
+
+        else:
+            with open(self.filename, "ab") as dbfile:
+                dbfile.write(struct.pack("<QH", self.hash(key), len(value)))
+                dbfile.write(value)
+
+        self.driftwood.log.info("Database", "put", "\"{0}\"".format(key))
+
+    def remove(self, key):
+        """Remove a key.
+
+        Removes a key and its value from the database.
+
+        Args:
+            key: The key to remove.
+        """
+        key = self.__test_str(key)
+        if not key:
+            return
+
+        pos, psz = self.__get_pos(key)
+
+        if pos >= 0:
+            with open(self.filename, "rb") as dbfile:
+                with open(self.filename+'~', "wb") as tmpfile:
+                    tmpfile.write(dbfile.read(pos))
+                    dbfile.seek(10+psz, 1)
+                    tmpfile.write(dbfile.read())
+
+            os.remove(self.filename)
+            os.rename(self.filename+'~', self.filename)
+
+            self.driftwood.log.info("Database", "remove", "\"{0}\"".format(key))
+
+        else:
+            self.driftwood.log.msg("ERROR", "Database", "no such key", "\"{0}\"".format(key))
+
+    def __get_pos(self, key):
+        """Get the byte position of a key in the database and the size of its value.
+        """
+        pos = -1
+        psz = 0
+
+        with open(self.filename, "rb") as dbfile:
+            while True:
+                ptmp = dbfile.tell()
+
+                block = dbfile.read(10)
+                if not block:
+                    break
+
+                stored_key, sz = struct.unpack("<QH", block)
+
+                if self.hash(key) == stored_key:
+                    pos = ptmp
+                    psz = sz
+                    break
+                else:
+                    dbfile.seek(sz, 1)
+
+            return (pos, psz)
+
+    def __test_str(self, value):
+        """Convert a value into a string if possible.
+        """
         if type(value) != str:
             try:
                 value = str(value)
+                return value
             except ValueError:
-                self.driftwood.log.msg("ERROR", "Database", "cannot convert value to string", value)
+                self.driftwood.log.msg("ERROR", "Database", "cannot convert to string", "\"{0}\"".format(value))
 
-        self.__database[item] = value
-
-    def __delitem__(self, item):
-        del self.__database[item]
-
-    def __iter__(self):
-        return self.__database.keys()
-
-    def __open(self, name):
-        """Open the database.
-
-        Args:
-            Basename of database to open.
-        """
-        try:
-            self.__database = dumbdbm.open(os.path.join(self.driftwood.config["database"]["root"], name), 'c')
-
-        except FileNotFoundError:
-            self.driftwood.log.msg("FATAL", "Database", "cannot open database in directory",
-                           self.driftwood.config["database"]["root"])
-            sys.exit(1)
-
-        self.driftwood.log.info("Database", "opened", name)
-
-    def __del__(self):
-        """DatabaseManager class destructor.
-        """
-        if self.__database:
-            self.__database.close()
+        else:
+            return value
