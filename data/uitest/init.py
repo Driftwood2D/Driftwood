040a555c81c341fe08e2c40758729fca770bc505
seisatsu <seisatsu@seisat.su>
2017-09-18 17:03:45 -0700

    Add uitest world, and remove test text from blue world.

---
+++ data/uitest/init.py
@@ -0,0 +1,14 @@
+# Driftwood 2D UI Testing Meta-World
+
+def init():
+    """Called on engine start.
+    """
+    # Set the logical resolution of the window.
+    Driftwood.window.resolution(160, 160)
+
+    # Load the area.
+    Driftwood.area.focus("grid.json")
+
+    # Run a UI test.
+    Driftwood.script["test01.py"].test()
+
---
+++ data/uitest/test01.py
@@ -0,0 +1,4 @@
+def test():
+    w = Driftwood.widget.insert_container(x=None, y=None, width=80, height=80)
+    Driftwood.widget.insert_text("Test Text", "pf_arma_five.ttf", 16, parent=w, x=None, y=None,
+		                 width=None, height=None, color="0000FFFF", active=True)
