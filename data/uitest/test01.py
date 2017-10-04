e34d2b3477c5b3f0a8df97c01973d0f0e658b129
seisatsu <seisatsu@seisat.su>
2017-10-04 00:53:00 -0700

    More Jinja2 examplery.

--- data/uitest/test01.py
+++ data/uitest/test01.py
@@ -1,7 +1,12 @@
 def test():
-    a = Driftwood.script["stdlib/widget.py"].load("widgets/textbox1.json",
-                        {"contents": "This is a textbox that has had its text wrapped a whole danged awful lot."})
-    b = Driftwood.script["stdlib/widget.py"].load("widgets/textbox2.json")
+    a = Driftwood.script["stdlib/widget.py"].load("widgets/textbox1.json", {
+        "contents": "This is a textbox that has had its text wrapped a whole danged awful lot.",
+        "wrap": 110
+    })
+    b = Driftwood.script["stdlib/widget.py"].load("widgets/textbox2.json", {
+        "contents": "This text will be replaced by the vars property.",
+        "line_height": "0.6"
+    })
 
     print(a)
     print(b)
